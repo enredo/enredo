017bc1342974561d33d176dda5c2aa24603ed81f
eptaccio <eptaccio@gmail.com>
2017-10-29 16:06:55 -0200

    refactor: criado bin, e agora utilizando child_process execFile para executar o arquivo

---
+++ cli.js
@@ -0,0 +1,12 @@
+#!/ usr/bin/env node
+
+const bin = require('./index')
+
+const ENR_FILE_PATH = process.argv[2]
+
+bin.execute({enrFilePath: ENR_FILE_PATH}, (error, result) => {
+  if (error) {
+    return console.error(error)
+  }
+  console.log(result)
+})
--- index.js
+++ index.js
@@ -1,46 +1,60 @@
 const fs = require('fs')
 const replaceValues = require('./replace')
 const path = require('path')
+const {execFile} = require('child_process')
 
 const encodingOptions = {
   encoding: 'utf-8'
 }
 
-const ENR_FILE_PATH = process.argv[2]
 const BUILD_PATH = path.join(__dirname, 'build.js')
 
-const getFile = () => fs.readFileSync(ENR_FILE_PATH, encodingOptions)
+const getFile = ({filePath, encodingOptions}) => fs.readFileSync(filePath, encodingOptions)
 
 const build = ({originalFile, replaceValues, originalKey, newValueKey}) => {
   let file = originalFile
+
   replaceValues.forEach(replaceValue => {
     const regex = new RegExp(replaceValue[originalKey], 'g')
     file = file.replace(regex, replaceValue[newValueKey])
   })
+
   return file
 }
 
-const write = (file) => fs.writeFileSync(BUILD_PATH, file, encodingOptions)
+const write = ({file, buildPath, encodingOptions}) => fs.writeFileSync(buildPath, file, encodingOptions)
 
-try {
-  const enrFile = getFile()
-
-  const jsFile = build({
-    originalFile: enrFile,
-    replaceValues: replaceValues,
-    originalKey: 'enr',
-    newValueKey: 'js'
-  })
-
-  write(jsFile)
-  require(BUILD_PATH)
-} catch (error) {
-  const errorInEnrSyntax = build({
+const buildErrorInEnrSyntax = (error) => {
+  return build({
     originalFile: error.stack,
-    replaceValues: replaceValues,
+    replaceValues,
     originalKey: 'js',
     newValueKey: 'enr'
   })
+}
 
-  console.error(errorInEnrSyntax)
+const buildJsFile = (enrFile) => {
+  return build({
+    originalFile: enrFile,
+    replaceValues,
+    originalKey: 'enr',
+    newValueKey: 'js'
+  })
+}
+
+const execute = ({enrFilePath}, done) => {
+  const enrFile = getFile({filePath: enrFilePath, encodingOptions})
+
+  const jsFile = buildJsFile(enrFile)
+
+  write({file: jsFile, buildPath: BUILD_PATH, encodingOptions})
+
+  execFile('node', [BUILD_PATH], (error, stdout, stderr) => {
+    if (error) return done(buildErrorInEnrSyntax(error))
+    done(null, stdout)
+  })
+}
+
+module.exports = {
+  execute
 }
